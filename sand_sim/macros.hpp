67f2c8b86c1f8234f564df16175f9417002d3a2f
noryzenn <hemidovhesenbey@gmail.com>
2026-03-16 19:20:38 +0400

    Update macros.hpp

--- sand_sim/macros.hpp
+++ sand_sim/macros.hpp
@@ -1,18 +1,3 @@
-// Crucial steps:
-
-// 1 - draw a rectangle, in random position
-// 2 - learn mouse drag and getting position, and according to that draw rectangles
-// 3 - use 2d array to store data
-// 4 - if down part of sand is 0, make it 1 and make current 0. always start from bottom ig
-
-// Notes:
-
-// 28/10/25 update, seems like i gotta implement the array first, then ill keep the data. according to that data ill draw rectangles
-// js learnt that i cant initialize a dynamic memory type inside class, i can declare it but initializing should be in the constructor
-
-// 29/10/25 finished implementation, just add some things for decoration
-
-
 #ifndef MACROS_H
 #define MACROS_H
 
